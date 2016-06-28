790ce1389a824b57f2458c41d334e42e95b4ea3b
Nicola <bombonati.nicola@gmail.com>
2016-06-28 16:06:35 +0200

    modded

--- src/main/java/hello/Greeter.java
+++ src/main/java/hello/Greeter.java
@@ -1,6 +1,15 @@
 package hello;
 
+
+import model.FirstSingleton;
+
+import javax.inject.Inject;
+
 public class Greeter {
+
+    @Inject
+    FirstSingleton Ston;
+
     public String sayHello() {
         return "Hello world!";
     }
